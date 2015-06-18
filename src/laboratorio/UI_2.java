1bf8349b80f1dd58a03515fdf860fdb3a5af42b0
Cristian <Cristian@Equipo_Flores>
2015-06-18 08:48:32 -0600

    se agrego los botones de eliminar y editar, a los formularios UI_2 e UI_1 el de editar envia una persona al metodo editar en la clase conexionEspesifica y el de eliminar envia el id al metodo eliminar en la clse conexionExpecifica

--- src/laboratorio/UI_2.java
+++ src/laboratorio/UI_2.java
@@ -55,6 +55,8 @@ public class UI_2 extends javax.swing.JFrame implements Observador{
         jTextField2 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -79,6 +81,20 @@ public class UI_2 extends javax.swing.JFrame implements Observador{
 
         jLabel3.setText("Apellido:");
 
+        jButton2.setText("Eliminar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Editar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -97,7 +113,12 @@ public class UI_2 extends javax.swing.JFrame implements Observador{
                         .addComponent(jTextField2)
                         .addComponent(jTextField3)
                         .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING)))
+                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 408, Short.MAX_VALUE)
                 .addContainerGap())
@@ -129,7 +150,11 @@ public class UI_2 extends javax.swing.JFrame implements Observador{
                                     .addComponent(jLabel3)
                                     .addGap(38, 38, 38))
                                 .addComponent(jLabel4)))
-                        .addGap(0, 115, Short.MAX_VALUE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton3)
+                        .addGap(0, 57, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -155,6 +180,37 @@ public class UI_2 extends javax.swing.JFrame implements Observador{
             JOptionPane.showMessageDialog(null, "No deje ningun campo vacio.");
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        id=Integer.parseInt(jTextField1.getText().trim());
+        try {
+            ce.eliminar(id);
+            JOptionPane.showMessageDialog(null, "¡Eliminacion Exitosa!");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        id=Integer.parseInt(jTextField1.getText());
+        nombre=jTextField2.getText();
+        apellido=jTextField3.getText();
+        conectado = jComboBox1.getSelectedItem().equals("Si");
+        if (nombre != null && apellido != null && conectado != null) {
+            try {
+                ce.editar(new Persona(id, nombre, apellido, conectado));
+                JOptionPane.showMessageDialog(null, "¡Modificacion Exitosa!");
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+            jTextField1.setText("");jTextField2.setText("");jTextField3.setText("");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "No deje ningun campo vacio.");
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -193,6 +249,8 @@ public class UI_2 extends javax.swing.JFrame implements Observador{
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
